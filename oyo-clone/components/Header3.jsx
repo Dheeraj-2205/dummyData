2643cec8e747d32b28b3fb5114018061b018e66b
Dheeraj Joshi <djsays124@gmail.com>
2023-12-16 01:58:19 +0530

    Add files via upload

--- oyo-clone/components/Header3.jsx
+++ oyo-clone/components/Header3.jsx
@@ -21,22 +21,13 @@ const Header3 = () => {
               setCity(e.target.value);
             }}
           />
-          {/* <input
-            type="date"
-            placeholder="Search..."
-            className="  h-16 outline-none px-3 text-lg border-r-2 border-gray-400 col-span-1"
-          />
-          <input
-            type="date"
-            placeholder="Search..."
-            className=" h-16 outline-none px-3 text-lg col-span-1"
-          /> */}
+
 
           <button
             type="submit"
             className=" h-16 px-3 py-2 w-72 bg-green-400 hover:cursor-pointer hover:bg-green-600 text-white text-xl"
           >
-            <Link href={`/hotels?city=${city}`}>Search</Link>
+            <Link href={`/api/hotels?city=${city}`}>Search</Link>
           </button>
         </div>
         <div className="flex mx-20 my-5 font-bold">
